8aa6cba5cec03542abadc52ac0737e37d5500772
Paul Cryer-Whitehead <paul.cryer-whitehead@ukho.gov.uk>
2020-02-21 16:08:37 +0000

    more formatting

--- tests/test_gsf_swathbathyping.py
+++ tests/test_gsf_swathbathyping.py
@@ -12,29 +12,29 @@ from gsfpy.enums import FileMode, RecordType, SeekOption
 
 
 class TestGsfSwathBathyPing(unittest.TestCase):
-
     def setUp(self):
-        test_data_306_path = path.join(path.dirname(__file__),
-                                       "0029_20160323_185603_EX1604_MB.gsf.mb121")
-        self.test_data_306 = {'path': test_data_306_path, 'num_beams': 432}
-        test_data_307_path = path.join(path.dirname(__file__),
-                                       "0059_20181102_212138_EX1811_MB_EM302.gsf.mb121")
-        self.test_data_307 = {'path': test_data_307_path, 'num_beams': 432}
+        test_data_306_path = path.join(
+            path.dirname(__file__), "0029_20160323_185603_EX1604_MB.gsf.mb121"
+        )
+        self.test_data_306 = {"path": test_data_306_path, "num_beams": 432}
+        test_data_307_path = path.join(
+            path.dirname(__file__), "0059_20181102_212138_EX1811_MB_EM302.gsf.mb121"
+        )
+        self.test_data_307 = {"path": test_data_307_path, "num_beams": 432}
 
     def tearDown(self):
         tmp_files = os.listdir(tempfile.gettempdir())
-        temp_gsf_files = [f for f in tmp_files if f.startswith('temp_gsf')]
+        temp_gsf_files = [f for f in tmp_files if f.startswith("temp_gsf")]
         for gsf_file in temp_gsf_files:
             try:
                 os.remove(path.join(tempfile.gettempdir(), gsf_file))
             except IOError as io_err:
-                print(f'Unable to delete temp file {gsf_file} : {str(io_err)}')
+                print(f"Unable to delete temp file {gsf_file} : {str(io_err)}")
 
     def test_gsf_swathbathyping_read(self):
-        gsf_file = gsfpy.open_gsf(self.test_data_306['path'], FileMode.GSF_READONLY)
+        gsf_file = gsfpy.open_gsf(self.test_data_306["path"], FileMode.GSF_READONLY)
         data_id, gsf_records = gsf_file.read(
-            RecordType.GSF_RECORD_SWATH_BATHYMETRY_PING,
-            1
+            RecordType.GSF_RECORD_SWATH_BATHYMETRY_PING, 1
         )
         gsf_file.close()
 
@@ -42,41 +42,40 @@ class TestGsfSwathBathyPing(unittest.TestCase):
 
         num_beams = swath_bathymetry_ping_record.number_beams
 
-        assertpy.assert_that(num_beams).is_equal_to(self.test_data_306['num_beams'])
+        assertpy.assert_that(num_beams).is_equal_to(self.test_data_306["num_beams"])
         beam_angles = swath_bathymetry_ping_record.beam_angle[:num_beams]
 
         beam_angles_in_range = [180 >= x >= -180 for x in beam_angles]
         assertpy.assert_that(False).is_not_in(beam_angles_in_range)
 
     def test_gsf_swathbathyping_write_update_sequential(self):
-        tmp_file_path = '/tmp/temp_gsf_306_test_data_update.gsf'
-        shutil.copyfile(self.test_data_306['path'], tmp_file_path)
+        tmp_file_path = "/tmp/temp_gsf_306_test_data_update.gsf"
+        shutil.copyfile(self.test_data_306["path"], tmp_file_path)
         gsf_file = gsfpy.open_gsf(tmp_file_path, FileMode.GSF_UPDATE)
         record_num = 0
         data_id, gsf_records = gsf_file.read(
-            RecordType.GSF_RECORD_SWATH_BATHYMETRY_PING,
-            record_num)
+            RecordType.GSF_RECORD_SWATH_BATHYMETRY_PING, record_num
+        )
 
         num_beams = gsf_records.mb_ping.number_beams
 
-        assertpy.assert_that(num_beams).is_equal_to(self.test_data_306['num_beams'])
+        assertpy.assert_that(num_beams).is_equal_to(self.test_data_306["num_beams"])
 
         original_values = gsf_records.mb_ping.beam_flags[:num_beams]
 
-        update_beams = {'3': 1, '4': 1, '5': 1, '6': 1}
+        update_beams = {"3": 1, "4": 1, "5": 1, "6": 1}
         for i in update_beams.keys():
             gsf_records.mb_ping.beam_flags[int(i)] = c_ubyte(update_beams[i])
 
         gsf_file.seek(SeekOption.GSF_PREVIOUS_RECORD)
-        gsf_file.write(gsf_records,
-                       RecordType.GSF_RECORD_SWATH_BATHYMETRY_PING,
-                       record_num)
+        gsf_file.write(
+            gsf_records, RecordType.GSF_RECORD_SWATH_BATHYMETRY_PING, record_num
+        )
         gsf_file.close()
 
         reopened_gsf_file = gsfpy.open_gsf(tmp_file_path, FileMode.GSF_READONLY)
         read_data_id, read_gsf_records = reopened_gsf_file.read(
-            RecordType.GSF_RECORD_SWATH_BATHYMETRY_PING,
-            record_num
+            RecordType.GSF_RECORD_SWATH_BATHYMETRY_PING, record_num
         )
         read_ping_record = read_gsf_records.mb_ping
 
@@ -87,33 +86,32 @@ class TestGsfSwathBathyPing(unittest.TestCase):
                 self.assertEqual(original_values[i], read_ping_record.beam_flags[i])
 
     def test_gsf_swathbathyping_update_by_index(self):
-        tmp_file_path = '/tmp/temp_gsf_306_test_data_update_idx.gsf'
-        shutil.copyfile(self.test_data_306['path'], tmp_file_path)
+        tmp_file_path = "/tmp/temp_gsf_306_test_data_update_idx.gsf"
+        shutil.copyfile(self.test_data_306["path"], tmp_file_path)
         gsf_file = gsfpy.open_gsf(tmp_file_path, FileMode.GSF_UPDATE_INDEX)
         record_num = 3
         data_id, gsf_records = gsf_file.read(
-            RecordType.GSF_RECORD_SWATH_BATHYMETRY_PING,
-            record_num)
+            RecordType.GSF_RECORD_SWATH_BATHYMETRY_PING, record_num
+        )
 
         num_beams = gsf_records.mb_ping.number_beams
 
-        assertpy.assert_that(num_beams).is_equal_to(self.test_data_306['num_beams'])
+        assertpy.assert_that(num_beams).is_equal_to(self.test_data_306["num_beams"])
 
         original_values = gsf_records.mb_ping.beam_flags[:num_beams]
 
-        update_beams = {'3': 1, '4': 1, '5': 1, '6': 1}
+        update_beams = {"3": 1, "4": 1, "5": 1, "6": 1}
         for i in update_beams.keys():
             gsf_records.mb_ping.beam_flags[int(i)] = c_ubyte(update_beams[i])
 
-        gsf_file.write(gsf_records,
-                       RecordType.GSF_RECORD_SWATH_BATHYMETRY_PING,
-                       record_num)
+        gsf_file.write(
+            gsf_records, RecordType.GSF_RECORD_SWATH_BATHYMETRY_PING, record_num
+        )
         gsf_file.close()
 
         reopened_gsf_file = gsfpy.open_gsf(tmp_file_path, FileMode.GSF_READONLY_INDEX)
         read_data_id, read_gsf_records = reopened_gsf_file.read(
-            RecordType.GSF_RECORD_SWATH_BATHYMETRY_PING,
-            record_num
+            RecordType.GSF_RECORD_SWATH_BATHYMETRY_PING, record_num
         )
         read_ping_record = read_gsf_records.mb_ping
         updated_flags = read_ping_record.beam_flags[:num_beams]
@@ -125,8 +123,7 @@ class TestGsfSwathBathyPing(unittest.TestCase):
                 self.assertEqual(original_values[i], read_ping_record.beam_flags[i])
 
         first_data_id, first_gsf_records = reopened_gsf_file.read(
-            RecordType.GSF_RECORD_SWATH_BATHYMETRY_PING,
-            1  # 1-indexed
+            RecordType.GSF_RECORD_SWATH_BATHYMETRY_PING, 1  # 1-indexed
         )
         first_rec_flags = first_gsf_records.mb_ping.beam_flags[:num_beams]
         self.assertNotEqual(first_rec_flags, updated_flags)
@@ -139,7 +136,7 @@ class TestGsfSwathBathyPing(unittest.TestCase):
         self._gsf_swathbathyping_read_by_index(self.test_data_307)
 
     def _gsf_swathbathyping_read_by_index(self, test_data_info):
-        gsf_file = gsfpy.open_gsf(test_data_info['path'], FileMode.GSF_READONLY)
+        gsf_file = gsfpy.open_gsf(test_data_info["path"], FileMode.GSF_READONLY)
 
         # Read a couple of records forwards, so its at position 3.
         # Records are 1-indexed.
@@ -157,15 +154,15 @@ class TestGsfSwathBathyPing(unittest.TestCase):
 
         num_beams = swath_bathymetry_ping_record.number_beams
 
-        assertpy.assert_that(num_beams).is_equal_to(test_data_info['num_beams'])
+        assertpy.assert_that(num_beams).is_equal_to(test_data_info["num_beams"])
         beam_angles = swath_bathymetry_ping_record.beam_angle[:num_beams]
         beam_flags = swath_bathymetry_ping_record.beam_flags[:num_beams]
 
-        gsf_file_by_index = gsfpy.open_gsf(test_data_info['path'],
-                                           FileMode.GSF_READONLY_INDEX)
+        gsf_file_by_index = gsfpy.open_gsf(
+            test_data_info["path"], FileMode.GSF_READONLY_INDEX
+        )
         index_data_id, gsf_index_records = gsf_file_by_index.read(
-            RecordType.GSF_RECORD_SWATH_BATHYMETRY_PING,
-            record_index
+            RecordType.GSF_RECORD_SWATH_BATHYMETRY_PING, record_index
         )
 
         record_by_index = gsf_index_records.mb_ping
--- tests/test_gsfpy_swathbathy_records.py
+++ tests/test_gsfpy_swathbathy_records.py
@@ -17,11 +17,12 @@ from gsfpy.timespec import c_timespec
 
 
 class TestGsfpySwathBathyRecords(unittest.TestCase):
-
     def setUp(self):
 
-        test_data_306_path = path.join(os.fsencode(path.dirname(__file__)),
-                                       b"0029_20160323_185603_EX1604_MB.gsf.mb121")
+        test_data_306_path = path.join(
+            os.fsencode(path.dirname(__file__)),
+            b"0029_20160323_185603_EX1604_MB.gsf.mb121",
+        )
 
         self.test_data_306_path = test_data_306_path
         self.test_data_306 = (test_data_306_path, 432)
@@ -32,28 +33,34 @@ class TestGsfpySwathBathyRecords(unittest.TestCase):
         end_ts = c_timespec(tv_sec=c_int32(end_time), tv_nsec=c_long(509))
         # TODO: tv_nsec should be c_int according to filespec. Check.
 
-        self.summary_to_save = {'min_lat': -14.02349, 'max_lat': 34.089,
-                                'min_long': 3.089234, 'max_long': 3.235,
-                                'min_depth': 3.23, 'max_depth': 93.23,
-                                'start_time': start_ts, 'end_time': end_ts
-                                }
+        self.summary_to_save = {
+            "min_lat": -14.02349,
+            "max_lat": 34.089,
+            "min_long": 3.089234,
+            "max_long": 3.235,
+            "min_depth": 3.23,
+            "max_depth": 93.23,
+            "start_time": start_ts,
+            "end_time": end_ts,
+        }
 
     def tearDown(self):
         temp_files = os.listdir(tempfile.gettempdir())
-        temp_gsf_files = [f for f in temp_files if f.startswith('temp_gsf')]
+        temp_gsf_files = [f for f in temp_files if f.startswith("temp_gsf")]
         for gsf_file in temp_gsf_files:
             try:
                 os.remove(path.join(tempfile.gettempdir(), gsf_file))
             except IOError as io_err:
-                print(f'Unable to delete temp file {gsf_file} : {str(io_err)}')
+                print(f"Unable to delete temp file {gsf_file} : {str(io_err)}")
 
     def test_gsf_swath_summary(self):
         c_int_ptr = POINTER(c_int)
         p_gsf_fileref = c_int_ptr(c_int(0))
 
         swath_summary_id = c_gsfDataID()
-        swath_summary_id.recordID = \
-            c_uint(RecordType.GSF_RECORD_SWATH_BATHY_SUMMARY.value)
+        swath_summary_id.recordID = c_uint(
+            RecordType.GSF_RECORD_SWATH_BATHY_SUMMARY.value
+        )
 
         c_gsfDataID_ptr = POINTER(c_gsfDataID)
         p_dataID = c_gsfDataID_ptr(swath_summary_id)
@@ -64,23 +71,24 @@ class TestGsfpySwathBathyRecords(unittest.TestCase):
         c_ubyte_ptr = POINTER(c_ubyte)
         p_stream = c_ubyte_ptr()
 
-        expected_start = datetime(year=2016, month=3, day=23,
-                                  hour=18, minute=56, second=3)
-        expected_end = datetime(year=2016, month=3, day=23,
-                                hour=18, minute=57, second=16)
+        expected_start = datetime(
+            year=2016, month=3, day=23, hour=18, minute=56, second=3
+        )
+        expected_end = datetime(
+            year=2016, month=3, day=23, hour=18, minute=57, second=16
+        )
 
         # Read from file
-        open_return_value = gsfpy.bindings.gsfOpen(self.test_data_306_path,
-                                                   FileMode.GSF_READONLY,
-                                                   p_gsf_fileref
-                                                   )
+        open_return_value = gsfpy.bindings.gsfOpen(
+            self.test_data_306_path, FileMode.GSF_READONLY, p_gsf_fileref
+        )
         bytes_read = gsfpy.bindings.gsfRead(
             c_int(p_gsf_fileref[0]),
             c_int(RecordType.GSF_RECORD_SWATH_BATHY_SUMMARY.value),
             p_dataID,
             p_record,
             p_stream,
-            0
+            0,
         )
         close_return_value = gsfpy.bindings.gsfClose(c_int(p_gsf_fileref[0]))
 
@@ -114,8 +122,7 @@ class TestGsfpySwathBathyRecords(unittest.TestCase):
         p_gsf_fileref = c_int_ptr(c_int(0))
 
         gsf_data_id = c_gsfDataID()
-        gsf_data_id.recordID = \
-            c_uint(RecordType.GSF_RECORD_SWATH_BATHYMETRY_PING.value)
+        gsf_data_id.recordID = c_uint(RecordType.GSF_RECORD_SWATH_BATHYMETRY_PING.value)
 
         c_gsfDataID_ptr = POINTER(c_gsfDataID)
         p_dataID = c_gsfDataID_ptr(gsf_data_id)
@@ -126,17 +133,16 @@ class TestGsfpySwathBathyRecords(unittest.TestCase):
         c_ubyte_ptr = POINTER(c_ubyte)
         p_stream = c_ubyte_ptr()
 
-        open_return_value = gsfpy.bindings.gsfOpen(self.test_data_306_path,
-                                                   mode,
-                                                   p_gsf_fileref
-                                                   )
+        open_return_value = gsfpy.bindings.gsfOpen(
+            self.test_data_306_path, mode, p_gsf_fileref
+        )
         bytes_read = gsfpy.bindings.gsfRead(
             c_int(p_gsf_fileref[0]),
             c_int(RecordType.GSF_RECORD_SWATH_BATHYMETRY_PING.value),
             p_dataID,
             p_record,
             p_stream,
-            0
+            0,
         )
         close_return_value = gsfpy.bindings.gsfClose(c_int(p_gsf_fileref[0]))
 
@@ -156,7 +162,7 @@ class TestGsfpySwathBathyRecords(unittest.TestCase):
         assertpy.assert_that(False).is_not_in(beam_angles_in_range)
 
     def test_gsf_swathsummary_save_update(self):
-        tmp_file_path = '/tmp/temp_gsf_306_test_data_update.gsf'
+        tmp_file_path = "/tmp/temp_gsf_306_test_data_update.gsf"
         shutil.copyfile(self.test_data_306_path, tmp_file_path)
         file_mode = FileMode.GSF_UPDATE
 
@@ -164,8 +170,9 @@ class TestGsfpySwathBathyRecords(unittest.TestCase):
         p_gsf_fileref = c_int_ptr(c_int(0))
 
         swath_summary_id = c_gsfDataID()
-        swath_summary_id.recordID = \
-            c_uint(RecordType.GSF_RECORD_SWATH_BATHY_SUMMARY.value)
+        swath_summary_id.recordID = c_uint(
+            RecordType.GSF_RECORD_SWATH_BATHY_SUMMARY.value
+        )
 
         c_gsfDataID_ptr = POINTER(c_gsfDataID)
         p_dataID = c_gsfDataID_ptr(swath_summary_id)
@@ -177,33 +184,34 @@ class TestGsfpySwathBathyRecords(unittest.TestCase):
         p_stream = c_ubyte_ptr()
 
         # Read the record that will be amended
-        open_return_value = gsfpy.bindings.gsfOpen(os.fsencode(tmp_file_path),
-                                                   file_mode,
-                                                   p_gsf_fileref
-                                                   )
+        open_return_value = gsfpy.bindings.gsfOpen(
+            os.fsencode(tmp_file_path), file_mode, p_gsf_fileref
+        )
         bytes_read = gsfpy.bindings.gsfRead(
             c_int(p_gsf_fileref[0]),
             c_int(RecordType.GSF_RECORD_SWATH_BATHY_SUMMARY.value),
             p_dataID,
             p_record,
-            p_stream
+            p_stream,
         )
 
         summary = p_record.contents.summary
-        summary.start_time = self.summary_to_save['start_time']
-        summary.end_time = self.summary_to_save['end_time']
-        summary.max_depth = c_double(self.summary_to_save['max_depth'])
-        summary.min_depth = c_double(self.summary_to_save['min_depth'])
-        summary.min_latitude = c_double(self.summary_to_save['min_lat'])
-        summary.max_latitude = c_double(self.summary_to_save['max_lat'])
-        summary.min_longitude = c_double(self.summary_to_save['min_long'])
-        summary.max_longitude = c_double(self.summary_to_save['max_long'])
+        summary.start_time = self.summary_to_save["start_time"]
+        summary.end_time = self.summary_to_save["end_time"]
+        summary.max_depth = c_double(self.summary_to_save["max_depth"])
+        summary.min_depth = c_double(self.summary_to_save["min_depth"])
+        summary.min_latitude = c_double(self.summary_to_save["min_lat"])
+        summary.max_latitude = c_double(self.summary_to_save["max_lat"])
+        summary.min_longitude = c_double(self.summary_to_save["min_long"])
+        summary.max_longitude = c_double(self.summary_to_save["max_long"])
         # Go back to the first record so it doesn't write
         # the updated first summary over the top of the second one
-        seek_return_value = gsfpy.bindings.gsfSeek(c_int(p_gsf_fileref[0]),
-                                                   SeekOption.GSF_PREVIOUS_RECORD)
-        bytes_written = gsfpy.bindings.gsfWrite(c_int(p_gsf_fileref[0]),
-                                                p_dataID, p_record)
+        seek_return_value = gsfpy.bindings.gsfSeek(
+            c_int(p_gsf_fileref[0]), SeekOption.GSF_PREVIOUS_RECORD
+        )
+        bytes_written = gsfpy.bindings.gsfWrite(
+            c_int(p_gsf_fileref[0]), p_dataID, p_record
+        )
         close_return_value = gsfpy.bindings.gsfClose(c_int(p_gsf_fileref[0]))
 
         assertpy.assert_that(open_return_value).is_equal_to(0)
@@ -218,7 +226,8 @@ class TestGsfpySwathBathyRecords(unittest.TestCase):
         p_gsf_file_ref = c_int_ptr_read(c_int(0))
         gsf_read_data_id = c_gsfDataID()
         gsf_read_data_id.recordID = c_uint(
-            RecordType.GSF_RECORD_SWATH_BATHY_SUMMARY.value)
+            RecordType.GSF_RECORD_SWATH_BATHY_SUMMARY.value
+        )
 
         c_gsf_data_id_ptr = POINTER(c_gsfDataID)
         p_data_id = c_gsf_data_id_ptr(gsf_read_data_id)
@@ -230,9 +239,8 @@ class TestGsfpySwathBathyRecords(unittest.TestCase):
         p_rstream = c_ubyte_ptr()
 
         open_read_return_value = gsfpy.bindings.gsfOpen(
-            os.fsencode(tmp_file_path),
-            FileMode.GSF_READONLY,
-            p_gsf_file_ref)
+            os.fsencode(tmp_file_path), FileMode.GSF_READONLY, p_gsf_file_ref
+        )
 
         bytes_read = gsfpy.bindings.gsfRead(
             c_int(p_gsf_file_ref[0]),
@@ -240,24 +248,24 @@ class TestGsfpySwathBathyRecords(unittest.TestCase):
             p_data_id,
             p_read_record,
             p_rstream,
-            0
+            0,
         )
 
         read_summary = p_read_record.contents.summary
 
         close_return_value = gsfpy.bindings.gsfClose(c_int(p_gsf_file_ref[0]))
 
-        save_start_time = self.summary_to_save['start_time']
-        save_end_time = self.summary_to_save['end_time']
+        save_start_time = self.summary_to_save["start_time"]
+        save_end_time = self.summary_to_save["end_time"]
 
         self.assertEqual(0, open_read_return_value)
         self.assertEqual(48, bytes_read)
-        self.assertEqual(read_summary.min_depth, self.summary_to_save['min_depth'])
-        self.assertEqual(read_summary.max_depth, self.summary_to_save['max_depth'])
-        self.assertEqual(read_summary.min_latitude, self.summary_to_save['min_lat'])
-        self.assertEqual(read_summary.max_latitude, self.summary_to_save['max_lat'])
-        self.assertEqual(read_summary.min_longitude, self.summary_to_save['min_long'])
-        self.assertEqual(read_summary.max_longitude, self.summary_to_save['max_long'])
+        self.assertEqual(read_summary.min_depth, self.summary_to_save["min_depth"])
+        self.assertEqual(read_summary.max_depth, self.summary_to_save["max_depth"])
+        self.assertEqual(read_summary.min_latitude, self.summary_to_save["min_lat"])
+        self.assertEqual(read_summary.max_latitude, self.summary_to_save["max_lat"])
+        self.assertEqual(read_summary.min_longitude, self.summary_to_save["min_long"])
+        self.assertEqual(read_summary.max_longitude, self.summary_to_save["max_long"])
         self.assertEqual(save_start_time.tv_sec, read_summary.start_time.tv_sec)
         self.assertEqual(save_end_time.tv_sec, read_summary.end_time.tv_sec)
         self.assertEqual(save_start_time.tv_nsec, read_summary.start_time.tv_nsec)
@@ -265,8 +273,9 @@ class TestGsfpySwathBathyRecords(unittest.TestCase):
         self.assertEqual(0, close_return_value)
 
     def test_gsf_swathsummary_save_create(self):
-        tmp_file_path = path.join(tempfile.gettempdir(),
-                                  'temp_gsf_306_test_data_create.gsf')
+        tmp_file_path = path.join(
+            tempfile.gettempdir(), "temp_gsf_306_test_data_create.gsf"
+        )
 
         file_mode = FileMode.GSF_CREATE
 
@@ -274,8 +283,9 @@ class TestGsfpySwathBathyRecords(unittest.TestCase):
         p_gsf_fileref = c_int_ptr(c_int(0))
 
         swath_summary_id = c_gsfDataID()
-        swath_summary_id.recordID = \
-            c_uint(RecordType.GSF_RECORD_SWATH_BATHY_SUMMARY.value)
+        swath_summary_id.recordID = c_uint(
+            RecordType.GSF_RECORD_SWATH_BATHY_SUMMARY.value
+        )
 
         c_gsfDataID_ptr = POINTER(c_gsfDataID)
         p_dataID = c_gsfDataID_ptr(swath_summary_id)
@@ -283,31 +293,29 @@ class TestGsfpySwathBathyRecords(unittest.TestCase):
         c_gsfRecords_ptr = POINTER(c_gsfRecords)
         p_record = c_gsfRecords_ptr(c_gsfRecords())
 
-        ts_start = self.summary_to_save['start_time']
-        ts_end = self.summary_to_save['end_time']
+        ts_start = self.summary_to_save["start_time"]
+        ts_end = self.summary_to_save["end_time"]
 
         summary = c_gsfSwathBathySummary(
             start_time=ts_start,
             end_time=ts_end,
-            min_latitude=c_double(self.summary_to_save['min_lat']),
-            max_latitude=c_double(self.summary_to_save['max_lat']),
-            min_longitude=c_double(self.summary_to_save['min_long']),
-            max_longitude=c_double(self.summary_to_save['max_long']),
-            min_depth=c_double(self.summary_to_save['min_depth']),
-            max_depth=c_double(self.summary_to_save['max_depth'])
-            )
+            min_latitude=c_double(self.summary_to_save["min_lat"]),
+            max_latitude=c_double(self.summary_to_save["max_lat"]),
+            min_longitude=c_double(self.summary_to_save["min_long"]),
+            max_longitude=c_double(self.summary_to_save["max_long"]),
+            min_depth=c_double(self.summary_to_save["min_depth"]),
+            max_depth=c_double(self.summary_to_save["max_depth"]),
+        )
 
         p_record.contents.summary = summary
 
         open_create_return_value = gsfpy.bindings.gsfOpen(
-            os.fsencode(tmp_file_path),
-            file_mode,
-            p_gsf_fileref
+            os.fsencode(tmp_file_path), file_mode, p_gsf_fileref
         )
 
-        bytes_written = gsfpy.bindings.gsfWrite(c_int(p_gsf_fileref[0]),
-                                                p_dataID,
-                                                p_record)
+        bytes_written = gsfpy.bindings.gsfWrite(
+            c_int(p_gsf_fileref[0]), p_dataID, p_record
+        )
         close_return_value = gsfpy.bindings.gsfClose(c_int(p_gsf_fileref[0]))
 
         self.assertEqual(0, open_create_return_value)
@@ -319,7 +327,8 @@ class TestGsfpySwathBathyRecords(unittest.TestCase):
         p_gsf_file_ref = c_int_ptr_read(c_int(0))
         gsf_read_data_id = c_gsfDataID()
         gsf_read_data_id.recordID = c_uint(
-            RecordType.GSF_RECORD_SWATH_BATHYMETRY_PING.value)
+            RecordType.GSF_RECORD_SWATH_BATHYMETRY_PING.value
+        )
 
         c_gsf_data_id_ptr = POINTER(c_gsfDataID)
         p_data_id = c_gsf_data_id_ptr(gsf_read_data_id)
@@ -330,16 +339,16 @@ class TestGsfpySwathBathyRecords(unittest.TestCase):
         c_ubyte_ptr = POINTER(c_ubyte)
         p_stream = c_ubyte_ptr()
 
-        open_read_return_value = gsfpy.bindings.gsfOpen(os.fsencode(tmp_file_path),
-                                                        FileMode.GSF_READONLY,
-                                                        p_gsf_file_ref)
+        open_read_return_value = gsfpy.bindings.gsfOpen(
+            os.fsencode(tmp_file_path), FileMode.GSF_READONLY, p_gsf_file_ref
+        )
         bytes_read = gsfpy.bindings.gsfRead(
             c_int(p_gsf_file_ref[0]),
             c_int(RecordType.GSF_RECORD_SWATH_BATHY_SUMMARY.value),
             p_data_id,
             p_read_record,
             p_stream,
-            0
+            0,
         )
         close_read_return_value = gsfpy.bindings.gsfClose(c_int(p_gsf_file_ref[0]))
 
@@ -353,15 +362,21 @@ class TestGsfpySwathBathyRecords(unittest.TestCase):
 
         assertpy.assert_that(read_start_time).is_equal_to(ts_start.tv_sec)
         assertpy.assert_that(read_end_time).is_equal_to(ts_end.tv_sec)
-        assertpy.assert_that(read_summary.min_depth)\
-            .is_equal_to(self.summary_to_save['min_depth'])
-        assertpy.assert_that(read_summary.max_depth)\
-            .is_equal_to(self.summary_to_save['max_depth'])
-        assertpy.assert_that(read_summary.min_latitude)\
-            .is_equal_to(self.summary_to_save['min_lat'])
-        assertpy.assert_that(read_summary.max_latitude)\
-            .is_equal_to(self.summary_to_save['max_lat'])
-        assertpy.assert_that(read_summary.min_longitude)\
-            .is_equal_to(self.summary_to_save['min_long'])
-        assertpy.assert_that(read_summary.max_longitude)\
-            .is_equal_to(self.summary_to_save['max_long'])
+        assertpy.assert_that(read_summary.min_depth).is_equal_to(
+            self.summary_to_save["min_depth"]
+        )
+        assertpy.assert_that(read_summary.max_depth).is_equal_to(
+            self.summary_to_save["max_depth"]
+        )
+        assertpy.assert_that(read_summary.min_latitude).is_equal_to(
+            self.summary_to_save["min_lat"]
+        )
+        assertpy.assert_that(read_summary.max_latitude).is_equal_to(
+            self.summary_to_save["max_lat"]
+        )
+        assertpy.assert_that(read_summary.min_longitude).is_equal_to(
+            self.summary_to_save["min_long"]
+        )
+        assertpy.assert_that(read_summary.max_longitude).is_equal_to(
+            self.summary_to_save["max_long"]
+        )
