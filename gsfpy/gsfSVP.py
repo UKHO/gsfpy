0a15a29b1dc43e15d7f46fc6134d4f389c9d5e73
lmarsden <luke.marsden@ukho.gov.uk>
2020-03-21 12:34:39 +0000

    Update gsfNavigationError and gsfSVP for GSF 3.08
    * Order of latitude and longitude fields has changed in these
      structures in v3.08 of the GSF library. Wrapper code updated
      to reflect this.

--- gsfpy/gsfSVP.py
+++ gsfpy/gsfSVP.py
@@ -7,8 +7,8 @@ class c_gsfSVP(Structure):
     _fields_ = [
         ("observation_time", timespec.c_timespec),
         ("application_time", timespec.c_timespec),
-        ("longitude", c_double),
         ("latitude", c_double),
+        ("longitude", c_double),
         ("number_points", c_int),
         ("depth", POINTER(c_double)),
         ("sound_speed", POINTER(c_double)),
