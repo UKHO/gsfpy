6ae041a9895c42c4314e4a877950bfdb1cbd8cea
lmarsden <luke.marsden@ukho.gov.uk>
2020-03-26 18:35:08 +0000

    Add remaining sensor-specific types

--- gsfpy/gsfBRBIntensity.py
+++ gsfpy/gsfBRBIntensity.py
@@ -5,6 +5,7 @@ from ctypes import (
     c_char,
     c_double,
     c_int,
+    c_short,
     c_ubyte,
     c_uint,
     c_ushort,
@@ -24,10 +25,6 @@ class c_gsfTimeSeriesIntensity(Structure):
     ]
 
 
-# NOTE - the largest of the imagery-specific types is defined here, to ensure the
-# gsfSensorImagery union type has the correct size (see
-# https://stackoverflow.com/questions/740577/sizeof-a-union-in-c-c). Other types in
-# the union will be defined at a later date.
 R2_SONIC_12_BYTE_STRING = c_ubyte * 12
 R2_SONIC_SPARE_BYTES = c_ubyte * 32
 R2_SONIC_MORE_INFO = c_double * 6
@@ -64,25 +61,84 @@ class c_gsfR2SonicImagerySpecific(Structure):
     ]
 
 
-# FIXME - Define classes for other imagery-specific tyes
+EM3IMG_SPARE = c_ubyte * 4
+
+
 class c_gsfEM3ImagerySpecific(Structure):
-    _fields_ = [("FIXME", c_int)]
+    _fields_ = [
+        ("range_norm", c_ushort),
+        ("start_tvg_ramp", c_ushort),
+        ("stop_tvg_ramp", c_ushort),
+        ("bsn", c_char),
+        ("bso", c_char),
+        ("mean_absorption", c_double),
+        ("offset", c_short),
+        ("scale", c_short),
+        ("spare", EM3IMG_SPARE),
+    ]
+
+
+RESON7100IMG_SPARE = c_ubyte * 64
 
 
 class c_gsfReson7100ImagerySpecific(Structure):
-    _fields_ = [("FIXME", c_int)]
+    _fields_ = [
+        ("size", c_ushort),
+        ("spare", RESON7100IMG_SPARE),
+    ]
+
+
+RESON8100IMG_SPARE = c_ubyte * 8
 
 
 class c_gsfReson8100ImagerySpecific(Structure):
-    _fields_ = [("FIXME", c_int)]
+    _fields_ = [
+        ("spare", RESON8100IMG_SPARE),
+    ]
+
+
+RESONTSERIESIMG_SPARE = c_ubyte * 64
+
+
+class c_gsfResonTSeriesImagerySpecific(Structure):
+    _fields_ = [
+        ("size", c_ushort),
+        ("spare", RESONTSERIESIMG_SPARE),
+    ]
+
+
+EM4IMG_SPARE = c_ubyte * 20
 
 
 class c_gsfEM4ImagerySpecific(Structure):
-    _fields_ = [("FIXME", c_int)]
+    _fields_ = [
+        ("sampling_frequency", c_double),
+        ("mean_absorption", c_double),
+        ("tx_pulse_length", c_double),
+        ("range_norm", c_int),
+        ("start_tvg_ramp", c_int),
+        ("stop_tvg_ramp", c_int),
+        ("bsn", c_double),
+        ("bso", c_double),
+        ("tx_beam_width", c_double),
+        ("tvg_cross_over", c_double),
+        ("offset", c_short),
+        ("scale", c_short),
+        ("spare", EM4IMG_SPARE),
+    ]
+
+
+KLEIN5410IMG_BEAM = c_uint * 5
+KLEIN5410IMG_SPARE = c_ubyte * 4
 
 
 class c_gsfKlein5410BssImagerySpecific(Structure):
-    _fields_ = [("FIXME", c_int)]
+    _fields_ = [
+        ("res_mode", c_uint),
+        ("tvg_page", c_uint),
+        ("beam_id", KLEIN5410IMG_BEAM),
+        ("spare", KLEIN5410IMG_SPARE),
+    ]
 
 
 class c_gsfSensorImagery(Union):
@@ -90,6 +146,7 @@ class c_gsfSensorImagery(Union):
         ("gsfEM3ImagerySpecific", c_gsfEM3ImagerySpecific),
         ("gsfReson7100ImagerySpecific", c_gsfReson7100ImagerySpecific),
         ("gsfReson8100ImagerySpecific", c_gsfReson8100ImagerySpecific),
+        ("gsfResonTSeriesImagerySpecific", c_gsfResonTSeriesImagerySpecific),
         ("gsfEM4ImagerySpecific", c_gsfEM4ImagerySpecific),
         ("gsfKlein5410BssImagerySpecific", c_gsfKlein5410BssImagerySpecific),
         ("gsfR2SonicImagerySpecific", c_gsfR2SonicImagerySpecific),
