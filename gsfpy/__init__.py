5b95bc25b6b96a727ca71e4f54e1bf9c82a77de5
Luke Marsden <luke.marsden@ukho.gov.uk>
2021-03-01 13:45:04 +0000

    Rename parameter for improved clarity

--- gsfpy/__init__.py
+++ gsfpy/__init__.py
@@ -16,7 +16,9 @@ def get_default_gsf_version() -> str:
     return os.environ.get("DEFAULT_GSF_VERSION", "3.08").replace(".", "_")
 
 
-def mirror_default_gsf_version_submodule(_globals: dict, submodule_name: str = None):
+def mirror_default_gsf_version_submodule(
+    globals_dict: dict, submodule_name: str = None
+):
     """
     mirror_default_gsf_version_submodule() mirrors (via import) the version-specific
     module (from the appropriate gsfpyX_XX namespace) corresponding to the module from
@@ -27,9 +29,9 @@ def mirror_default_gsf_version_submodule(_globals: dict, submodule_name: str = N
     gsfpy.enums.
 
     Params:
-        _globals: the dictionary of globals pertaining to the calling module. This can
-                  be obtained with a call to globals(). This is required as the globals
-                  dictionary is updated by the call to the function.
+        globals_dict: the dictionary of globals pertaining to the calling module. This
+                      can be obtained with a call to globals(). This is required as the
+                      globals dictionary is updated by the call to the function.
         submodule_name: The name of the submodule to mirror from the gsfpyX_XX
                         namespace. If left empty, the top level gsfpyX_XX module will be
                         mirrored.
@@ -40,19 +42,19 @@ def mirror_default_gsf_version_submodule(_globals: dict, submodule_name: str = N
         filter(None, [f"gsfpy{get_default_gsf_version()}", submodule_name])
     )
 
-    gsf_submodule = __import__(gsf_submodule_name, _globals, None, ["*"], 0)
+    gsf_submodule = __import__(gsf_submodule_name, globals_dict, None, ["*"], 0)
 
     # The special attribute __all__ is a list of public members of the module, as
     # interpreted by import *. The globals collection must be updated with these
     # names to correctly mirror the submodule.
     if hasattr(gsf_submodule, "__all__"):
-        _globals.update(
+        globals_dict.update(
             (name, getattr(gsf_submodule, name)) for name in gsf_submodule.__all__
         )
     else:
         # In cases where there is no "__all__" perform the equivalent by importing
         # all non-private members (i.e. those that do not start with an underscore).
-        _globals.update(
+        globals_dict.update(
             (name, getattr(gsf_submodule, name))
             for name in dir(gsf_submodule)
             if not name.startswith("_")
